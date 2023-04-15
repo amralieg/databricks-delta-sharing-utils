1c58919656342d4b889c8e9c78c8ef0ae443eff0
amr.ali@databricks.com <amr.ali@databricks.com>
2023-04-15 17:04:16 +0000

    updated examples

--- examples/example_data_provider.py
+++ examples/example_data_provider.py
@@ -4,10 +4,11 @@
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC drop database if exists hive_metastore.__delta_share_test975_ cascade;
-# MAGIC create database hive_metastore.__delta_share_test975_;
-# MAGIC create table hive_metastore.__delta_share_test975_.table1 (id int, name string);
-# MAGIC insert into hive_metastore.__delta_share_test975_.table1 values (1, "a"), (2, "b"), (3, "c");
+# MAGIC drop catalog if exists __deltasharing_test_975__ cascade;
+# MAGIC create catalog __deltasharing_test_975__;
+# MAGIC create database __deltasharing_test_975__.__delta_share_test975__;
+# MAGIC create table __deltasharing_test_975__.__delta_share_test975__.table1 (id int, name string);
+# MAGIC insert into __deltasharing_test_975__.__delta_share_test975__.table1 values (1, "a"), (2, "b"), (3, "c");
 
 # COMMAND ----------
 
@@ -16,26 +17,11 @@
 dsp = DeltaShareProvider(share="my_share", recipient="my_recipient", drop_share_if_exists=True, drop_recipient_if_exists=True)
 
 # share a table with Change Data Feed enabled so the data recipient can incrementally load the data
-dsp.share_table(table="hive_metastore.__delta_share_test975_.table1", enable_cdf=True)
+dsp.share_table(table="__deltasharing_test_975__.__delta_share_test975__.table1", enable_cdf=True)
 
 # COMMAND ----------
 
 # MAGIC %sql 
-# MAGIC update hive_metastore.__delta_share_test975_.table1 set name="d" where id=3;
-# MAGIC delete from hive_metastore.__delta_share_test975_.table1 where id=1;
-# MAGIC insert into hive_metastore.__delta_share_test975_.table1 values (4, "d"), (5, "e"), (6, "f");
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC ALTER SHARE my_share ADD TABLE hive_metastore.__delta_share_test975_.table1 WITH HISTORY;
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC describe catalog hive_metastore
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC describe share my_share;
+# MAGIC update __deltasharing_test_975__.__delta_share_test975__.table1 set name="d" where id=3;
+# MAGIC delete from __deltasharing_test_975__.__delta_share_test975__.table1 where id=1;
+# MAGIC insert into __deltasharing_test_975__.__delta_share_test975__.table1 values (4, "d"), (5, "e"), (6, "f");
